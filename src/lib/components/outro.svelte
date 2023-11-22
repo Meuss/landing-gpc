c171a2432436e017e49c91d472bb0cd2397888fe
Thomas Miller <thomas.miller147@gmail.com>
2023-11-22 21:54:07 +0100

    logos

--- src/lib/components/outro.svelte
+++ src/lib/components/outro.svelte
@@ -1,5 +1,7 @@
 <script>
 	import { email } from '$lib/variables';
+	import pc_gruyere from '$lib/assets/pc_gruyere.svg';
+	import pc_gruyere_2 from '$lib/assets/pc_gruyere_2.svg';
 </script>
 
 <section>
@@ -11,4 +13,16 @@
 		>
 	</p>
 	<h2 class="lg:text-center lg:mt-20">À bientôt sur les courts!</h2>
+
+	<div class="border-2 border-secondary rounded bg-slate-50 p-4 text-center max-w-2xl mx-auto">
+		<h3>--- sera enlevé ---</h3>
+		<p class="text-sm">
+			@Maxime et comité, un ami (vrai designer, pas comme moi) s'est amusé à faire des logos hier,
+			vous en pensez quoi?
+		</p>
+		<div class="flex flex-col justify-around items-center gap-5 sm:max-h-72 sm:flex-row">
+			<img src={pc_gruyere} class="w-40 sm:w-50" alt="" />
+			<img src={pc_gruyere_2} class="w-40 sm:w-50" alt="" />
+		</div>
+	</div>
 </section>
